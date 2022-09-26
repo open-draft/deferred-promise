05e7e938f540bcedd7c740cab0d58032e3657b07
Artem Zakharchenko <kettanaito@gmail.com>
2022-09-26 12:48:35 +0200

    fix: improve methods chaining compatibility (#2)

--- test/DeferredPromise.test.ts
+++ test/DeferredPromise.test.ts
@@ -1,36 +1,82 @@
 import { DeferredPromise } from "../src";
 
-it('can be listened to with ".then()"', (done) => {
-  expect.assertions(1);
+describe("Promise-compliance", () => {
+  it('can be listened to with ".then()"', (done) => {
+    expect.assertions(1);
 
-  const promise = new DeferredPromise<number>();
+    const promise = new DeferredPromise<number>();
 
-  promise.then((data) => {
+    promise.then((data) => {
+      expect(data).toBe(123);
+      done();
+    });
+
+    promise.resolve(123);
+  });
+
+  it('can be listened to with ".catch()"', (done) => {
+    expect.assertions(1);
+
+    const promise = new DeferredPromise<number>();
+    promise.catch((reason) => {
+      expect(reason).toBe("error");
+      done();
+    });
+
+    promise.reject("error");
+  });
+
+  it("can be awaited with async/await", async () => {
+    const promise = new DeferredPromise<number>();
+    promise.resolve(123);
+
+    const data = await promise;
     expect(data).toBe(123);
-    done();
   });
 
-  promise.resolve(123);
-});
+  it('allows data transformation in the ".then()" chain', async () => {
+    const promise = new DeferredPromise<number>();
 
-it('can be listened to with ".catch()"', (done) => {
-  expect.assertions(1);
+    promise.then((value) => value * 2).then((value) => value + 10);
+    promise.resolve(5);
 
-  const promise = new DeferredPromise<number>();
-  promise.catch((reason) => {
-    expect(reason).toBe("error");
-    done();
+    const number = await promise;
+
+    expect(number).toBe(20);
   });
 
-  promise.reject("error");
-});
+  it('allows ".catch().then()" chaining', async () => {
+    const promise = new DeferredPromise<number>();
 
-it("can be awaited", async () => {
-  const promise = new DeferredPromise<number>();
-  promise.resolve(123);
+    promise
+      .catch<number>((value) => {
+        if (typeof value === "number") {
+          return value;
+        }
+      })
+      .then((value) => value + 10);
 
-  const data = await promise;
-  expect(data).toBe(123);
+    promise.reject(5);
+    const number = await promise;
+
+    expect(number).toBe(15);
+  });
+
+  it('does not alter resolved data with ".finally()"', async () => {
+    const promise = new DeferredPromise<number>();
+
+    const finallyCallback = jest.fn(() => "unexpected");
+    const wrapper = (): Promise<number> => {
+      return promise.finally(finallyCallback);
+    };
+
+    promise.resolve(123);
+    const result = await wrapper();
+
+    expect(result).toBe(123);
+    expect(finallyCallback).toHaveBeenCalledTimes(1);
+    expect(finallyCallback).toHaveBeenCalledWith();
+  });
 });
 
 describe("resolve()", () => {
