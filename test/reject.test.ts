6883b2d8bad3105d5c30963104274306fd5456b9
Artem Zakharchenko <kettanaito@gmail.com>
2022-11-24 14:03:40 +0100

    chore: use rejects assertion in "reject" tests

--- test/reject.test.ts
+++ test/reject.test.ts
@@ -2,27 +2,26 @@ import { createDeferredExecutor } from '../src/createDeferredExecutor'
 
 it('can be rejected without any reason', async () => {
   const executor = createDeferredExecutor<void>()
-  const promise = new Promise<void>(executor).catch(() => {})
+  const promise = new Promise<void>(executor)
   expect(executor.state).toBe('pending')
 
   executor.reject()
   expect(executor.state).toBe('pending')
 
-  expect(await promise).toBeUndefined()
+  await expect(promise).rejects.toBeUndefined()
   expect(executor.state).toBe('rejected')
   expect(executor.rejectionReason).toBeUndefined()
 })
 
 it('can be rejected with a reason', async () => {
   const executor = createDeferredExecutor<void>()
-  const promise = new Promise<void>(executor).catch(() => {})
+  const promise = new Promise<void>(executor)
   expect(executor.state).toBe('pending')
 
   const rejectionReason = new Error('Something went wrong')
   executor.reject(rejectionReason)
   expect(executor.state).toBe('pending')
 
-  expect(await promise).toBeUndefined()
+  await expect(promise).rejects.toThrowError(rejectionReason)
   expect(executor.state).toBe('rejected')
-  expect(executor.rejectionReason).toEqual(rejectionReason)
 })
